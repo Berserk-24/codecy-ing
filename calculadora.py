523a6e547364dd8c23725ece11ddc98735edce4e
Berserk-24 <mitgard04@gmail.com>
2025-05-18 17:08:45 -0500

    prueba5

--- calculadora.py
+++ calculadora.py
@@ -5,5 +5,13 @@ def dividir(a, b):
     return a / b 
  
 saludar("Mundo") 
+
 print(dividir(10, 0)) 
 
+def dividir(a, b):
+    try:
+        R = a / b
+    except ZeroDivisionError:
+        print("no se puede dividir entre cero")
+        R = None
+    return R
--- utilidades.py
+++ utilidades.py
@@ -7,15 +7,5 @@ def restar(a, b):
 def multiplicar(a, b): 
     return a * b 
  
-def dividir(a, b): 
-    if b == 0: 
-        return "Error: división por cero" 
-    return a / b 
-def operacion_innecesaria(x): 
-    if x > 0: 
-        return True 
-    else: 
-        return True 
-    
-    
+
 
