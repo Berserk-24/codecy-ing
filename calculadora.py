a834895ed6c69e45b6e261f403f823420d0b2d09
Berserk-24 <mitgard04@gmail.com>
2025-05-18 17:55:36 -0500

    pruebaF

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,36 @@
-def saludar(nombre): 
-    print("Hola " + nombre) 
- 
-saludar("Mundo") 
+import os  
 
-def dividir(a, b):
-    try:
-        R = a / b
-    except ZeroDivisionError:
-        print("no se puede dividir entre cero")
-        R = None
-    return R
+def Saludar(Nombre): 
+  print("Hola " + Nombre)
 
-print(dividir(10, 0)) 
+def sumar(a,b): return a+b  
+
+def funcion_duplicada(x):
+    if x > 5:
+        return True
+    else:
+        return False
+
+def funcion_duplicada(x): 
+    if x > 5:
+        return True
+    else:
+        return False
+
+def funcion_larga():
+    a = 1
+    b = 2
+    c = 3
+    d = 4
+    e = 5
+    f = 6
+    g = 7
+    h = 8
+    i = 9
+    j = 10
+    k = 11
+    l = 12
+    m = 13
+    n = 14
+    o = 15
+    return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o
